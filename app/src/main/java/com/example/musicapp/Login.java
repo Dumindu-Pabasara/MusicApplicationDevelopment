e9510e9dffa48bc596b2181e27aba7e8676617cb
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-05-31 10:26:25 +0530

    Update Login.java

--- app/src/main/java/com/example/musicapp/Login.java
+++ app/src/main/java/com/example/musicapp/Login.java
@@ -1,47 +1,22 @@
 package com.example.musicapp;
 
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.musicapp.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-private ActivityLoginBinding binding;
+    Button getStartBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-     binding = ActivityLoginBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        getStartBtn = findViewById(R.id.getStartBtn);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        getStartBtn.setOnClickListener(view ->
+                Toast.makeText(Login.this, "Starting app...", Toast.LENGTH_SHORT).show()
+        );
     }
 }
