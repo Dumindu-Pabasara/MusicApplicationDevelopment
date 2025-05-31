7056ac54c4b8d3f4f056f5eda0f9c26861e2be4f
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-06-01 00:51:37 +0530

    Update Login.java

--- app/src/main/java/com/example/musicapp/Login.java
+++ app/src/main/java/com/example/musicapp/Login.java
@@ -1,23 +1,62 @@
+//package com.example.musicapp;
+//
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.Toast;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class Login extends AppCompatActivity {
+//    Button getStartBtn;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//
+//        getStartBtn = findViewById(R.id.loginButton);
+//
+//        getStartBtn.setOnClickListener(view ->
+//                Toast.makeText(Login.this, "Starting app...", Toast.LENGTH_SHORT).show()
+//
+//        );
+//
+//    }
+//}
+
 package com.example.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
     Button getStartBtn;
+    TextView signUpText; // signup textView එක declare කරන්න
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Log In button action
         getStartBtn = findViewById(R.id.loginButton);
-
         getStartBtn.setOnClickListener(view ->
                 Toast.makeText(Login.this, "Starting app...", Toast.LENGTH_SHORT).show()
-
         );
+
+        // Sign Up text click එක handle කරන්න
+        signUpText = findViewById(R.id.signUpText);
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MainActivity2 එකට යන්න
+                Intent intent = new Intent(Login.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
